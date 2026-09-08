f50e3b109b4855c78f85516487f26bdcfc947ac6
Beltsys <86432957+beltsys-labs@users.noreply.github.com>
2026-09-08 09:33:50 +0200

    fix(build): globales UMD de @tokelia/web3-* en rollup.globals.js

--- rollup.globals.js
+++ rollup.globals.js
@@ -1,15 +1,11 @@
 export default {
+  '@tokelia/web3-blockchains': 'Web3Blockchains',
+  '@tokelia/web3-client': 'Web3Client',
+  '@tokelia/web3-tokens': 'Web3Tokens',
+  '@tokelia/web3-exchanges': 'Web3Exchanges',
+  '@tokelia/web3-wallets': 'Web3Wallets',
+  '@tokelia/web3-payments': 'Web3Payments',
   '@depay/solana-web3.js': 'SolanaWeb3js',
-  '@depay/web3-blockchains': 'Web3Blockchains',
-  '@depay/web3-client': 'Web3Client',
-  '@depay/web3-client-evm': 'Web3Client',
-  '@depay/web3-client-svm': 'Web3Client',
-  '@depay/web3-exchanges': 'Web3Exchanges',
-  '@depay/web3-exchanges-evm': 'Web3Exchanges',
-  '@depay/web3-exchanges-svm': 'Web3Exchanges',
-  '@depay/web3-tokens': 'Web3Tokens',
-  '@depay/web3-tokens-evm': 'Web3Tokens',
-  '@depay/web3-tokens-svm': 'Web3Tokens',
   'decimal.js': 'Decimal',
   'ethers': 'ethers'
 }
